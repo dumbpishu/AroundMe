ada83ad6ead3c3b6f5b4a0b5f7cbf04921323d56
Priyanshu Singh <priyanshuparashar08@gmail.com>
2026-03-19 16:02:36 +0530

    create user controller & service

--- server/src/controllers/user.controller.ts
+++ server/src/controllers/user.controller.ts
@@ -1,7 +1,6 @@
 import { Request, Response } from "express";
-import { currentUserService, updateUserDetailsService, updateUserAvatarService } from "../services/user.service";
+import { currentUserService, updateUserDetailsService, updateUserAvatarService, deleteUserService } from "../services/user.service";
 import { uploadBufferToCloudinary } from "../services/cloudinary.service";
-import { uploadToCloudinary } from "./upload.controller";
 
 export const currentUser = async (req: Request, res: Response) => {
     try {
@@ -42,16 +41,34 @@ export const updateUserAvatar = async (req: Request, res: Response) => {
             return res.status(401).json({ success: false, message: "Unauthorized" });
         }
 
-        const file = req.file as Express.Multer.File;
+        const file = req.file;
         if (!file) {
             return res.status(400).json({ success: false, message: "No file uploaded" });
         }
 
-        const updatedResult = await uploadBufferToCloudinary(file, "aroundme/avatars");
+        if (!file.mimetype.startsWith("image/")) {
+            return res.status(400).json({ success: false, message: "Invalid file type. Only images are allowed." });
+        }
 
-        const result = await updateUserAvatarService(userId, updatedResult.url, updatedResult.public_id);
+        const uploadResult = await uploadBufferToCloudinary(file, "aroundme/avatars");
 
-        return res.status(200).json({ success: true, data: result, message: "User avatar updated successfully" });
+        const updatedUser = await updateUserAvatarService(userId, uploadResult?.url, uploadResult.public_id);
+
+        return res.status(200).json({ success: true, data: updatedUser, message: "User avatar updated successfully" });
+    } catch (error) {
+        return res.status(500).json({ success: false, message: "Server Error" });
+    }
+}
+
+export const deleteUser = async (req: Request, res: Response) => {
+    try {
+        const userId = req.user?.id;
+        if (!userId) {
+            return res.status(401).json({ success: false, message: "Unauthorized" });
+        }
+        await deleteUserService(userId);
+
+        return res.status(200).json({ success: true, data: {}, message: "User account deleted successfully" });
     } catch (error) {
         return res.status(500).json({ success: false, message: "Server Error" });
     }
--- server/src/services/user.service.ts
+++ server/src/services/user.service.ts
@@ -1,4 +1,5 @@
 import { User } from "../models/user.model";
+import { deleteFromCloudinary } from "./cloudinary.service";
 
 export const currentUserService = async (userId: string) => {
     const user = await User.findById(userId);
@@ -51,14 +52,19 @@ export const updateUserDetailsService = async (userId: string, updateData: { nam
     return payload;
 }
 
-export const updateUserAvatarService = async (userId: string, avatarUrl: string, publicId: string) => {
+export const updateUserAvatarService = async (userId: string, imageUrl: string, publicId: string) => {
     const user = await User.findById(userId);
 
     if (!user || user.isDeleted) {
         throw new Error("User not found.");
     }
 
-    user.avatar = avatarUrl;
+    if (user.avatarPublicId) {
+        await deleteFromCloudinary(user.avatarPublicId);
+    }
+
+    user.avatar = imageUrl;
+    user.avatarPublicId = publicId;
     await user.save();
 
     const payload = {
@@ -69,7 +75,26 @@ export const updateUserAvatarService = async (userId: string, avatarUrl: string,
         avatar: user.avatar,
         createdAt: user.createdAt,
         updatedAt: user.updatedAt
-    }
+    };
 
     return payload;
+}
+
+export const deleteUserService = async (userId: string) => {
+    const user = await User.findById(userId);
+
+    if (!user || user.isDeleted) {
+        throw new Error("User not found.");
+    }
+
+    if (user.avatarPublicId) {
+        await deleteFromCloudinary(user.avatarPublicId);
+    }
+
+    user.isDeleted = true;
+    user.email = `delete${user._id}@example.com`;
+    user.username = `deleted_user_${user._id}`;
+    await user.save();
+
+    return;
 }
