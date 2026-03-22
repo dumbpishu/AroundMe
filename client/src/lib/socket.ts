23a8ef88d412859e67351f1444a709c0966a61ab
Priyanshu Singh <priyanshuparashar08@gmail.com>
2026-03-22 12:30:30 +0530

    setup lib

--- client/src/lib/socket.ts
+++ client/src/lib/socket.ts
@@ -1,12 +1,6 @@
 import { io } from "socket.io-client";
 
-export const socket = io(
-  import.meta.env.VITE_SERVER_BASE_URL || "http://localhost:3000",
-  {
+export const socket = io(import.meta.env.VITE_API_BASE_URL || "http://localhost:8080", {
     withCredentials: true,
     autoConnect: false,
-    reconnection: true,
-    reconnectionAttempts: 5,
-    reconnectionDelay: 1000,
-  }
-);
+})
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,14 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { appRouter } from './routes/appRouter.tsx'
-import { RouterProvider } from 'react-router-dom'
-import { AuthProvider } from './context/AuthContext.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <AuthProvider>
-      <RouterProvider router={appRouter} />
-    </AuthProvider>
+    <div>Hello, World!</div>
   </StrictMode>,
 )
